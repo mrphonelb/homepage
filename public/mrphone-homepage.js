c45e3d782331f13af2f08b81d062a4364dbe2787
mrphonelb <sales@mrphonelb.com>
2026-06-25 23:48:15 +0300

    Update mrphone-homepage.js

--- public/mrphone-homepage.js
+++ public/mrphone-homepage.js
@@ -1,7 +1,20 @@
-console.log("Mr Phone Homepage JS loaded");
+console.log("Mr Phone Homepage Engine Loaded");
 
 window.MrPhone = {
-  test: function () {
-    console.log("MrPhone engine working");
-  }
+
+    api: "https://homepages-tr76.onrender.com",
+
+    async getProducts(cats, limit = 12) {
+
+        const url =
+            `${this.api}/api/homepage-section?cats=${cats.join(",")}&limit=${limit}`;
+
+        const response = await fetch(url);
+
+        if (!response.ok)
+            throw new Error("Cannot load products");
+
+        return await response.json();
+    }
+
 };
